0ead26a002e08b122710bc3cdc49cc341e40c4ea
rohiit0001 <2004rohitvishu@gmail.com>
2026-02-19 13:59:37 +0530

    update

--- index4a.js
+++ index4a.js
@@ -1,15 +1,30 @@
+// import express from "express"
+// const app = express()
+// app.listen(8080,()=>{
+//     console.log("server started")
+// })
+// app.get("/x/:a/y/:b/z/:c", (req, res) => {
+//   const num1 = Number(req.params.a);
+//   const num2 = Number(req.params.b);
+//   const num3 = Number(req.params.c);
+
+//   const sum = num1 + num2 + num3;
+
+//   res.send(sum);
+// });
+
+
+
+
+
 import express from "express"
 const app = express()
 app.listen(8080,()=>{
     console.log("server started")
-})
-app.get("/x/:a/y/:b/z/:c", (req, res) => {
-  const num1 = Number(req.params.a);
-  const num2 = Number(req.params.b);
-  const num3 = Number(req.params.c);
-
-  const sum = num1 + num2 + num3;
-
-  res.send("Sum = " + sum);
 });
+app.get("/:a/:b/:c/:d", (req, res) => {
+  res.send("Hello World");
+});
+
+
 
