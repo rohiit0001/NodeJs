a4240c1b72e1472d5279b3799fdc2729f40f73c6
rohiit0001 <2004rohitvishu@gmail.com>
2026-02-19 11:34:16 +0530

    update

---
+++ index2.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express()
+app.listen(8080,()=>{
+    console.log("server started")
+})
+app.get("/",(req,res)=>{
+    console.log(req.url)
+    res.send("response from server for /")
+
+})
---
+++ index3.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express()
+app.listen(8080,()=>{
+    console.log("server started")
+})
+app.get("/",(req,res)=>{
+console.log(req.url)
+res.send("response from server for /  ")
+})
+app.get("/home", (req, res) => {
+    console.log(req.url)
+  res.send("Response from server for /home page");
+});
